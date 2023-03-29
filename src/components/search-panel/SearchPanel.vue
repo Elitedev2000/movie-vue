6e5b337f8ed2ed0c93c2e867bf82f0c2c97a3be6
elitedev <elitedev2000@gmail.com>
2023-03-29 10:04:32 +0500

    Added

---
+++ src/components/search-panel/SearchPanel.vue
@@ -0,0 +1,20 @@
+<template>
+  <input
+    type="text"
+    class="form-control search-input"
+    placeholder="Kinolarni qidirish"
+  />
+</template>
+
+<script></script>
+
+<style scoped>
+.search-input {
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+  padding: 1.5rem;
+  border-radius: 4px;
+  background-color: #fcfaf5;
+  box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+}
+</style>
